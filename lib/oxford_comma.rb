11a42dfe02fe8753fcf58f959e6f651ae46cd5aa
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-07 22:26:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,15 @@
 def oxford_comma(array)
-  b = array.slice(-1).insert(0, ", and ")
-  case
+  case 
     when array.length == 1
       array.join
     when array.length == 2
       array.join(" and ")
     when array.length == 3
+      b = array.slice(-1).insert(0, ", and ")
       a = array.slice(0, 2).join(", ")
       a << b
     when array.length > 3
+      b = array.slice(-1).insert(0, ", and ")
       c = array.length - 1
       d = array.slice(0, c).join(", ")
       d << b
