76c21f97e950f372bc5e4d6ce84c9ad48750400a
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-07 22:23:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  b = array.slice(-1).insert(0, ", and ")
+  case 
+    when array.length == 1
+      array.join
+    when array.length == 2
+      array.join(" and ")
+    when array.length == 3
+      a = array.slice(0, 2).join(", ")
+      a << b
+    when array.length > 3
+      c = array.length - 1
+      d = array.slice(0, c).join(", ")
+      d << b
+    end
 end
